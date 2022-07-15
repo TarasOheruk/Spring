d3da12024ede4627d57eb78113899a9d9ec72b6f
TarasOheruk <tarikogeryk@gmail.com>
2022-07-15 10:26:05 +0300

    Add comment

--- src/main/java/com/example/registring/RegistringApplication.java
+++ src/main/java/com/example/registring/RegistringApplication.java
@@ -2,7 +2,7 @@ package com.example.registring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//hello from note
 @SpringBootApplication
 public class RegistringApplication {
 
